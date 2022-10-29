9ca555aefb24e6485412dc0391f2eae94f0a85f8
Akshat2106 <akshatpatidar2106@gmail.com>
2022-10-29 12:22:20 +0530

    dom5

---
+++ dom5.js
@@ -0,0 +1,62 @@
+var itemlist= document.querySelector('#items');
+// //parentnode
+// itemlist.parentNode.style.backgroundcolor='blue';
+
+// itemlist.parentElement.style.backgroundColor='green';
+// console.log(itemlist.parentElement.parentElement.parentElement);
+
+// //childrennode
+itemlist.children[1].style.backgroundColor='yellow';
+
+
+// //firstelementchild
+// itemlist.firstElementChild.textcontain='hello 1';
+// //lastchild
+// console.log(itemlist.lastChild;)
+// //lastelmentchild
+// itemlist.lastElementChild.textcontain='hello 4';
+
+// //nextsibling
+// console.log(itemlist.nextSibling);
+// console.log(itemlist.nextElementSibling);
+
+// //previoussibling
+// console.log(itemlist.previousSibling);
+// //previouselementsibling
+// itemlist.previousElementSibling.style.color='red';
+// //createelement
+// var newdiv =document.createElement('div');
+//  //add class
+//  newdiv.className='hello';
+//  //add id
+//  newdiv.id='hellohi';
+
+//  //add attibutes
+//  newdiv.setAttribute('title','hello div');
+
+//  //createtextnode
+//  var newdivtext=document.createTextNode('hello world');
+//  //add textto div
+//  newdiv.appendChild(newdivtext);
+
+//  var container=document.querySelector('header .container');
+//  var h1=document.querySelector('header h1');
+
+//  container.insertBefore(newdiv,h1);
+
+//  ////////////////////////////////////////////////////////////////
+ 
+//  //add HEllo word before Item Lister
+
+// var newdivtext=document.createTextNode('hello word');
+//  newdiv.appendChild(newdivtext);
+// var container=document.querySelector('header .container');
+// var h1=document.querySelector('header h1');
+//  container.insertBefore(newdiv,h1);
+//  //add HEllo word before Item 1
+//  var newdivtext=document.createTextNode('hello word');
+//  newdiv.appendChild(newdivtext);
+// var container=document.querySelector('li .list-group-item');
+// var h1=document.querySelector('li');
+//  container.insertBefore(newdiv,li);
+
