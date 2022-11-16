2cc969140fee7cb30d75abf704d17ac204b2dbde
Akshat2106 <akshatpatidar2106@gmail.com>
2022-11-16 17:30:25 +0530

    fat arrow

---
+++ fat arrow.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// let getA =a =>a;
+// console.log(getA(1));
+
+
+// let square=a=>a*a;
+// console.log(square(2));
+
+// let square=(a)=>{return a*a};
+// console.log(square(2));
+
+// var a=4;
+// let square= _ =>{return a*a};
+// console.log(square());
+
+let multi=(a,b)=>{return a*b};
+console.log(multi(2,4));
